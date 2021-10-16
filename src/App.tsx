985fbd2aab39e34a9d336a859c4f07a78b1ee307
Tiger <silavsale@gmail.com>
2021-10-16 19:46:47 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
 }
 
-export default App;
+function App() {
+  // const [people, setPeople] = useState([
+  //   {
+  //     name: "John Lennon",
+  //     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/John_Lennon_1969_%28cropped%29.jpg/440px-John_Lennon_1969_%28cropped%29.jpg",
+  //     age: 39,
+  //     note: "Singer songwriter musician",
+  //   },
+  //   {
+  //     name: "Paul McCartney",
+  //     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Paul_McCartney_in_October_2018.jpg/440px-Paul_McCartney_in_October_2018.jpg",
+  //     age: 79,
+  //     note: "Singer songwriter musician record and film producer businessman",
+  //   },
+  // ])
+
+  const [people, setPeople] = useState<IState["people"]>([])
+
+  return (
+    <div className="App">
+      <h1>The Beatles</h1>
+      {people.map((person) => (
+        <>
+          <p>{person.name}</p>
+        </>
+      ))}
+    </div>
+  )
+}
+
+export default App
