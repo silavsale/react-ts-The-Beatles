c9e6378d11d6d40b6558de733afad7b378ebdcf7
Tiger <silavsale@gmail.com>
2021-10-16 20:48:58 -0400

    created list and add to list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react"
 import "./App.css"
+import AddToList from "./components/AddToList"
+import List from "./components/List"
 
-interface IState {
+export interface IState {
   people: {
     name: string
     age: number
@@ -13,12 +15,6 @@ interface IState {
 function App() {
   // const [people, setPeople] = useState([
   //   {
-  //     name: "John Lennon",
-  //     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/John_Lennon_1969_%28cropped%29.jpg/440px-John_Lennon_1969_%28cropped%29.jpg",
-  //     age: 39,
-  //     note: "Singer songwriter musician",
-  //   },
-  //   {
   //     name: "Paul McCartney",
   //     url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/Paul_McCartney_in_October_2018.jpg/440px-Paul_McCartney_in_October_2018.jpg",
   //     age: 79,
@@ -26,16 +22,20 @@ function App() {
   //   },
   // ])
 
-  const [people, setPeople] = useState<IState["people"]>([])
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "John Lennon",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/John_Lennon_1969_%28cropped%29.jpg/440px-John_Lennon_1969_%28cropped%29.jpg",
+      age: 39,
+      note: "Singer songwriter musician",
+    },
+  ])
 
   return (
     <div className="App">
       <h1>The Beatles</h1>
-      {people.map((person) => (
-        <>
-          <p>{person.name}</p>
-        </>
-      ))}
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   )
 }
